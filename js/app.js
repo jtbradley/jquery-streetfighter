9c37c451604a57a9d4c154d6bca7c93bc80c4474
Jacob Bradley <jtbradley1@gmail.com>
2015-06-03 21:02:56 -0700

    cool pose

--- js/app.js
+++ js/app.js
@@ -24,10 +24,29 @@ $(document).ready(function(){
         $('.ryu-ready').show();
     });
 
+$(document).keydown(function(e) {
+        if(e.which == 88) {
+            playCool();
+            $('.ryu-throwing, .ryu-still, .ryu-ready').hide();
+            $('.ryu-cool').show();
+        }       
+    }).keyup(function(e) {
+        if(e.which == 88) {
+            $('#cool')[0].pause()
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
+    });
 });
+
 
 function playHadouken() {
         $('#hadouken-sound')[0].volume = 0.5;
         $('#hadouken-sound')[0].load();
         $('#hadouken-sound')[0].play();
     };
+
+function playCool() {
+    $('#cool')[0].load();
+    $('#cool')[0].play();
+}
